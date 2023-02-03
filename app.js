16184febbde02decbcb62c8c7e848ece7a61e423
Кирилл <kirill.denisenko.01@gmail.com>
2023-02-03 17:38:07 +0200

    feature added MongoDB in project

--- app.js
+++ app.js
@@ -1,88 +1,97 @@
-const fs = require("node:fs");
-
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const process = require("node:process");
 
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
 const app = express();
-
-function readBdFile(){
-    return JSON.parse(fs.readFileSync("books.json", "utf-8"))
-}
-
-function writeBdFile(booksBdFile, newBook){
-    return fs.writeFileSync('books.json', JSON.stringify([...booksBdFile, newBook]));
-}
-
 app.use(express.json());
 
-app.get('/books', function (req, res){
-    let books = readBdFile();
-    res.send(books)
+(async ()=>{
+    try {
+        await mongoClient.connect();
+        app.locals.collection = mongoClient.db("booksdb").collection("books");
+        app.listen(3000, ()=> console.log("Port 3000 is open....."))
+    } catch (err){
+        console.error(err)
+    }
+})();
+
+app.get('/books', async function (req, res){
+    const collection = req.app.locals.collection;
+    try{
+        const books = await collection.find({}).toArray();
+        res.send(books);
+    } catch (err){
+        console.log(err);
+        res.sendStatus(500);
+    }
 })
 
-app.get("/book/:id", function (req, res){
-    let books = readBdFile();
-    let bookId = req.params.id
-    let book = null;
-
-    books.forEach(function (item, index){
-        if(index === parseInt(bookId)){
-            book = item;
-        }
-    })
-    res.send(book)
-})
-
-app.put("/book/:id", function (req, res){
-    let books = readBdFile();
-    let body = req.body;
-    let indexBook = req.params.id;
-    books.forEach((book, index)=>{
-        if(book.id ===  parseInt(indexBook)){
-            books[index] = body
-        }
-    })
-    fs.writeFileSync("books.json", JSON.stringify(books));
-    res.send(`Books updated`);
-})
-
-app.delete("/book/:id", function (req, res){
-    let bookId = req.params.id;
-    let books = readBdFile();
-    let index = -1;
-    books.forEach(()=>{
-        if (books.id === parseInt(bookId)) {
-            index = books.id;
-        }
-    })
-    if( index > -1 ){
-        books.splice(index,1);
-        fs.writeFileSync("books.json",  JSON.stringify(books));
-        res.send("Successfully deleted")
-    } else {
-        res.status(400).send("Bad request");
+app.get("/books/:id", async function (req, res){
+    const collection = req.app.locals.collection;
+    const id = new ObjectId(req.params.id);
+    try{
+        const book = await collection.findOne({_id:id});
+        if(book) res.send(book);
+        else res.sendStatus(404);
+    } catch (err) {
+        console.error(err)
+        res.sendStatus(500);
     }
 
-
 })
 
-app.post("/book", function (req, res){
-    let booksBdFile = readBdFile();
-    let nameNewBook = req.body.name;
-    let authorNewBook = req.body.author;
-    let descriptionNewBook = req.body.description;
-    let idBooks = []
-    let newBook = { name: nameNewBook, author:authorNewBook, description:descriptionNewBook};
-    for (let i = 0; i < booksBdFile.length; i++){
-        idBooks.push(booksBdFile[i].id);
+app.put("/book/:id", async function (req, res){
+    if(!req.body) return res.sendStatus(400);
+    const collection = app.locals.collection;
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const result = await collection.findOneAndUpdate({_id:bookId},{
+            $set:{...req.body}},{returnDocument: "after"})
+        if (result.value) res.send(result.value);
+        else res.sendStatus(404);
+    } catch (err){
+        console.error(err)
+        res.sendStatus(500)
     }
-    if(idBooks.includes(booksBdFile.length))
-    {
-        newBook.id = booksBdFile.length + 1
-    }   else {
-        newBook.id = booksBdFile.length
-    }
-    writeBdFile(booksBdFile,newBook);
-    res.send(newBook)
 })
 
-app.listen(3000)
+app.delete("/book/:id",  async function (req, res){
+    const bookId = new ObjectId(req.params.id);
+    const collection = req.app.locals.collection;
+    try{
+        const result = await collection.findOneAndDelete({_id:bookId})
+        if(result.value) res.send(result.value);
+        else res.sendStatus(404);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500)
+    }
+})
+
+app.post("/books", async function (req, res){
+    if(!req.body) return res.sendStatus(400);
+    const collection = req.app.locals.collection;
+    const bookName = req.body.name;
+    const bookAuthor = req.body.author;
+    const bookDescription = req.body.description;
+    const book = {
+        name: bookName,
+        author:bookAuthor,
+        description:bookDescription
+    }
+    try {
+        await collection.insertOne(book);
+        res.send(book)
+    }
+     catch (err){
+        console.error(err);
+        res.sendStatus(500);
+    }
+})
+
+process.on("SIGINT",  async ()=>{
+    await mongoClient.close().then(()=> console.log("App close"))
+    process.exit();
+})
