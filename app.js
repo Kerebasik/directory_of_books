02e39a723c74cad95d90addcbbd559d1323965ee
Кирилл <kirill.denisenko.01@gmail.com>
2023-03-03 16:24:53 +0200

    Added github actions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./src/routes/bookRouter.js");
-const fileMulter = require('./middleware/multerConfig.js')
+const fileMulter = require('./src/middlewares/multerConfig.js')
 
 const PORT = 3000
 const DB_URL = "mongodb://127.0.0.1:27017/booksdb";
--- src/models/bookSchema.js
+++ src/models/bookSchema.js
@@ -6,7 +6,7 @@ const bookSchema = new mongoose.Schema( {
     name: { type: String, required:true },
     author: { type: String, required:true },
     description: { type:String },
-    tags: { type: [String], indexes: true },
+    tags: { type: [String] },
     image: { type: String }
 })
 
